c0be6a627cb050e39b0d04a63095795d5b2628e4
Geershati Saxena <geershatisaxena110@gmail.com>
2026-06-11 12:24:07 +0530

    j

--- script.js
+++ script.js
@@ -709,7 +709,7 @@ function initBackgroundMusic() {
     audio.style.display = 'none';
 
     var source = document.createElement('source');
-    source.src = '';
+    source.src = 'Songs/Hans Zimmer - Time (Official Audio).mp3';
     source.type = 'audio/mpeg';
 
     audio.appendChild(source);
